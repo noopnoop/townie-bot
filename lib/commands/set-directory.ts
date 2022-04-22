5532205ea9fd2d982a85c06e55f06b427e7db169
noops <redtinfoilcrane@gmail.com>
2022-04-22 16:36:53 -0700

    add typescript

---
+++ lib/commands/set-directory.ts
@@ -0,0 +1,42 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Permissions, MessageActionRow, MessageSelectMenu, GuildMember, CommandInteraction } from 'discord.js';
+
+const getChannels = async (interaction : CommandInteraction) => {
+  if (interaction.member instanceof GuildMember) {
+    const channels = await interaction.member?.guild.channels.fetch();
+    return channels.filter(channel => channel.type === 'GUILD_TEXT');
+  }
+};
+
+const makeChannelSelect = async (interaction : CommandInteraction) => {
+  const channels = await getChannels(interaction);
+  if (channels === undefined) return;
+  return new MessageActionRow()
+    .addComponents(
+      new MessageSelectMenu()
+        .setCustomId('set-directory')
+        .setPlaceholder('Nothing selected')
+        .addOptions(channels.map((channel) => {
+          return {
+            label: channel.name,
+            value: channel.id,
+          };
+        })),
+    );
+};
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('set-directory')
+    .setDescription('Designate a channel to be a directory for mafia games.'),
+  async execute (interaction : CommandInteraction) {
+    if (interaction.memberPermissions?.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      const channelSelect = await makeChannelSelect(interaction);
+      if (channelSelect) {
+        await interaction.reply({ components: [channelSelect], ephemeral: true, content: 'Choose a text channel to set as this server\'s mafia directory. Ideally, users apart from the Townie bot should not have permission to type in this channel.' });
+      }
+    } else {
+      await interaction.reply({ ephemeral: true, content: 'You must be an admininstrator to use this command.' });
+    }
+  },
+};
--- lib/index.ts
+++ lib/index.ts
@@ -1,25 +1,25 @@
-const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
+import fs from 'node:fs';
+import { Client, Collection, Intents, Interaction } from 'discord.js';
 const { token } = require('../config.json');
 
 const client = new Client ({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./lib/commands').filter(file => file.endsWith('.js'));
+const commands : Collection<String, any> = new Collection();
+const commandFiles = fs.readdirSync('./lib/commands').filter((file:String) => file.endsWith('.ts'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
+  commands.set(command.data.name, command);
 }
 
-const eventFiles = fs.readdirSync('./lib/events').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./lib/events').filter((file:String) => file.endsWith('.ts'));
 
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
   if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
+    client.once(event.name, (...args : any[]) => event.execute(...args));
   } else {
-    client.on(event.name, (...args) => event.execute(...args));
+    client.on(event.name, (...args : any[]) => event.execute(...args));
   }
 }
 
@@ -27,10 +27,10 @@ client.once('ready', () => {
   console.log('howdy');
 });
 
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction : Interaction) => {
   if (!interaction.isCommand()) return;
 
-  const command = client.commands.get(interaction.commandName);
+  const command = commands.get(interaction.commandName);
 
   if (!command) return;
 
