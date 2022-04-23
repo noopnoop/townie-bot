706cc4a786084e311daab1d5bf778384629f48b6
noops <redtinfoilcrane@gmail.com>
2022-04-22 19:02:45 -0700

    fix eslint

--- lib/index.ts
+++ lib/index.ts
@@ -1,18 +1,23 @@
 import fs from 'node:fs';
 import { Client, Collection, Intents, Interaction } from 'discord.js';
+import Keyv from 'keyv';
 const { token } = require('../config.json');
 
 const client = new Client ({ intents: [Intents.FLAGS.GUILDS] });
 
-const commands : Collection<String, any> = new Collection();
-const commandFiles = fs.readdirSync('./lib/commands').filter((file:String) => file.endsWith('.ts'));
+const directories = new Keyv('sqlite://../directories.sqlite');
+
+// gather up a Collection of command handlers
+const commands : Collection<string, any> = new Collection();
+const commandFiles = fs.readdirSync('./lib/commands').filter((file:string) => file.endsWith('.ts'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   commands.set(command.data.name, command);
 }
 
-const eventFiles = fs.readdirSync('./lib/events').filter((file:String) => file.endsWith('.ts'));
+// use event handlers
+const eventFiles = fs.readdirSync('./lib/events').filter((file:string) => file.endsWith('.ts'));
 
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
@@ -23,24 +28,23 @@ for (const file of eventFiles) {
   }
 }
 
-client.once('ready', () => {
-  console.log('howdy');
-});
-
+// handle interactions
 client.on('interactionCreate', async (interaction : Interaction) => {
-  if (!interaction.isCommand()) return;
+  // we try and use our Collection of command handlers for slash commands
+  if (interaction.isCommand()) {
+    const command = commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+      await command.execute(interaction);
+    }
+    catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
+    }
+  // handle setting a new directory
+  } else if (interaction.isSelectMenu()) {
 
-  const command = commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  }
-  catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command.', ephemeral: true });
-  }
+  } else {return;}
 });
 
 client.login(token);
