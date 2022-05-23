c247a023204a8043278bed125f6271b57e915622
noops <redtinfoilcrane@gmail.com>
2022-05-23 12:18:40 -0700

    night permissions

--- lib/types/game-data.ts
+++ lib/types/game-data.ts
@@ -26,6 +26,8 @@ export type GameState = Map<PlayerId, PlayerState>;
 export type Votes = Array<[PlayerId, Vote]>;
 
 export interface Game {
+  name : string
+  role : Role
   textChannel : TextChannel
   voiceChannel : VoiceChannel
   state: GameState
@@ -37,9 +39,9 @@ function makeGameStartMessage (roleId : string) {
   return { ephemeral: true, content: msg };
 }
 
-export function initialGameState (players : NormalMember[], textChannel: TextChannel, voiceChannel: VoiceChannel) : Game {
+export function initialGameState (listing : GameListing, textChannel: TextChannel, voiceChannel: VoiceChannel, role : Role) : Game {
   const state : GameState = new Map();
-  for (const player of players) {
+  for (const player of listing.players) {
     state.set(player.user.id, {
       name: player.displayName,
       team: 'Town',
@@ -47,6 +49,8 @@ export function initialGameState (players : NormalMember[], textChannel: TextCha
     });
   }
   return {
+    name : listing.name,
+    role : role,
     textChannel : textChannel,
     voiceChannel : voiceChannel,
     state: state,
@@ -55,20 +59,27 @@ export function initialGameState (players : NormalMember[], textChannel: TextCha
 }
 
 export async function setPermissionsBasedOnTime (time : Time, game : Game) {
-
+  let isAllowed : boolean;
+  if (time === 'Day') {
+    isAllowed = true
+  } else {
+    isAllowed = false
+  }
+  await game.textChannel.permissionOverwrites.edit(game.role,{
+    SEND_MESSAGES: isAllowed
+  })
+  await game.voiceChannel.permissionOverwrites.edit(game.role,{
+    SPEAK: isAllowed
+  })
 }
 
-export async function makeChannels (gameId : string, everyone : Role, mafiaRole : Role, channelManager : GuildChannelManager) {
+export async function makeChannels (gameId : string, everyone : Role, channelManager : GuildChannelManager) {
   const textChannel = await channelManager.create(gameId, {
     type: 'GUILD_TEXT',
     permissionOverwrites: [
       {
         id: everyone.id,
         deny: [ Permissions.FLAGS.SEND_MESSAGES ],
-      },
-      {
-        id: mafiaRole.id,
-        allow: [ Permissions.FLAGS.SEND_MESSAGES ],
       },
       {
         id: clientId,
@@ -83,28 +94,26 @@ export async function makeChannels (gameId : string, everyone : Role, mafiaRole 
         id: everyone.id,
         deny: [ Permissions.FLAGS.SPEAK],
       },
-      {
-        id: mafiaRole.id,
-        allow: [ Permissions.FLAGS.SPEAK ],
-      },
     ],
   });
   return {textChannel, voiceChannel};
 }
 
 export async function makeGame (listing: GameListing, roleManager : RoleManager, channelManager : GuildChannelManager, games: Games) {
-  const gameId = 'mafia-' + listing.creatorId;
+  const gameName = listing.name;
   const everyone = roleManager.everyone
   const mafiaRole = await roleManager.create({
-    name: gameId,
+    name: gameName,
   });
   for (const player of listing.players) {
     await player.roles.add(mafiaRole);
-    await sendRoleMessage(player);
+    //await sendRoleMessage(player); TURN THIS BACK ON LATER!!!!
   }
-  const {textChannel, voiceChannel} = await makeChannels(gameId,everyone,mafiaRole,channelManager);
+  const {textChannel, voiceChannel} = await makeChannels(gameName,everyone,channelManager);
   await textChannel.send(makeGameStartMessage(mafiaRole.id));
-  games.set(listing.creatorId, initialGameState(listing.players,textChannel, voiceChannel));
+  const newGame = initialGameState(listing,textChannel, voiceChannel, mafiaRole);
+  await setPermissionsBasedOnTime('Night', newGame);
+  games.set(listing.creatorId, newGame);
 }
 
 // export interface GameChannel {
