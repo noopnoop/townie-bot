75776cbc7754c3779ddb617873db45c10281ded1
noops <redtinfoilcrane@gmail.com>
2022-05-31 15:29:35 -0700

    make secret channel

--- lib/types/game-data.ts
+++ lib/types/game-data.ts
@@ -2,7 +2,7 @@ import { roleMention } from '@discordjs/builders';
 import { GuildChannelManager, RoleManager, Permissions, TextChannel, VoiceChannel, Role } from 'discord.js';
 import { ChannelId, GameListing, Games, NormalMember, PlayerId } from '../types';
 import { clientId } from '../config.json';
-import { sendRoleMessage } from './role-message';
+import { exec } from 'child_process'
 
 export interface GameData {
   players : [NormalMember],
@@ -19,19 +19,23 @@ export interface PlayerState {
   alive : boolean
 }
 
-export type Vote = PlayerId | 'Pass'
+export type VoteContent = PlayerId | 'Pass' | 'Unvote'
+
+export interface Vote {
+  content: VoteContent,
+  voter: PlayerId
+}
 
 export type GameState = Map<PlayerId, PlayerState>;
-
-export type Votes = Array<[PlayerId, Vote]>;
 
 export interface Game {
   name : string
   role : Role
   textChannel : TextChannel
+  secretTextChannel : TextChannel
   voiceChannel : VoiceChannel
   state: GameState
-  votes: Votes
+  votes: Vote[]
 }
 
 function makeGameStartMessage (roleId : string) {
@@ -39,7 +43,7 @@ function makeGameStartMessage (roleId : string) {
   return { ephemeral: true, content: msg };
 }
 
-export function initialGameState (listing : GameListing, textChannel: TextChannel, voiceChannel: VoiceChannel, role : Role) : Game {
+export function initialGameState (listing : GameListing, textChannel: TextChannel, secretTextChannel : TextChannel, voiceChannel: VoiceChannel, role : Role) : Game {
   const state : GameState = new Map();
   for (const player of listing.players) {
     state.set(player.user.id, {
@@ -52,10 +56,18 @@ export function initialGameState (listing : GameListing, textChannel: TextChanne
     name : listing.name,
     role : role,
     textChannel : textChannel,
+    secretTextChannel : secretTextChannel,
     voiceChannel : voiceChannel,
     state: state,
-    votes: new Array<[PlayerId, Vote]>(),
+    votes: new Array<Vote>()
   };
+}
+
+function gameToJson (game : Game) {
+  return JSON.stringify({
+    state: Object.fromEntries(game.state),
+    votes: game.votes
+  });
 }
 
 export async function setPermissionsBasedOnTime (time : Time, game : Game) {
@@ -71,6 +83,14 @@ export async function setPermissionsBasedOnTime (time : Time, game : Game) {
   await game.voiceChannel.permissionOverwrites.edit(game.role,{
     SPEAK: isAllowed
   })
+  for (const [player, state] of game.state) { //likely a better spot for this, needs testing as well
+    if (state.team === 'Mafia') {
+      await game.secretTextChannel.permissionOverwrites.edit(player,{
+        VIEW_CHANNEL: true,
+        SEND_MESSAGES: !isAllowed
+      })
+    }
+  }
 }
 
 export async function makeChannels (gameId : string, everyone : Role, channelManager : GuildChannelManager) {
@@ -87,6 +107,15 @@ export async function makeChannels (gameId : string, everyone : Role, channelMan
       },
     ],
   });
+  const secretTextChannel = await channelManager.create(gameId + '-secret', {
+    type: 'GUILD_TEXT',
+    permissionOverwrites: [
+      {
+        id: everyone.id,
+        deny: [ Permissions.FLAGS.VIEW_CHANNEL ],
+      },
+    ]
+  });
   const voiceChannel = await channelManager.create(gameId + '-voice', {
     type: 'GUILD_VOICE',
     permissionOverwrites: [
@@ -96,7 +125,8 @@ export async function makeChannels (gameId : string, everyone : Role, channelMan
       },
     ],
   });
-  return {textChannel, voiceChannel};
+  
+  return {textChannel, voiceChannel, secretTextChannel};
 }
 
 export async function makeGame (listing: GameListing, roleManager : RoleManager, channelManager : GuildChannelManager, games: Games) {
@@ -109,14 +139,13 @@ export async function makeGame (listing: GameListing, roleManager : RoleManager,
     await player.roles.add(mafiaRole);
     //await sendRoleMessage(player); TURN THIS BACK ON LATER!!!!
   }
-  const {textChannel, voiceChannel} = await makeChannels(gameName,everyone,channelManager);
+  const {textChannel, voiceChannel, secretTextChannel} = await makeChannels(gameName,everyone,channelManager);
   await textChannel.send(makeGameStartMessage(mafiaRole.id));
-  const newGame = initialGameState(listing,textChannel, voiceChannel, mafiaRole);
-  await setPermissionsBasedOnTime('Night', newGame);
+  const newGame = initialGameState(listing,textChannel, secretTextChannel, voiceChannel, mafiaRole);
+  await setPermissionsBasedOnTime('Day', newGame);
   games.set(listing.creatorId, newGame);
+  console.log(gameToJson(newGame));
+  // const townieProcess = exec('townie')
+  // if (!townieProcess || !townieProcess.stdin) throw new Error('error making game: something went wrong with townie')
+  // townieProcess.stdin.write(gameToJson(newGame));
 }
-
-// export interface GameChannel {
-//   id : ChannelId,
-
-// }
